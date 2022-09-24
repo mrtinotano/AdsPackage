b362b76c8b632f972f002c5ac955f4dcaad2bf4e
mrtinotano <adriamartinc@gmail.com>
2022-09-24 17:12:25 +0200

    Ads Manager

--- Assets/Runtime/Scripts/AdsManager.cs
+++ Assets/Runtime/Scripts/AdsManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utilities;
 
-public class AdsManager : MonoBehaviour
+namespace Ads
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AdsManager : Singleton<AdsManager>
     {
-        
-    }
+        protected override void Awake()
+        {
+            base.Awake();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            string appID = string.Empty;
+
+#if UNITY_ANDROID
+            appID = AppAdsSettings.Instance.AndroidAppID;
+#elif UNITY_IOS
+            appID = AppAdsSettings.Instance.iOSAppID;
+#endif
+
+            IronSource.Agent.init(appID);
+
+            if(AppAdsSettings.Instance.UseBanner)
+                IronSource.Agent.loadBanner(AppAdsSettings.Instance.BannerSize, AppAdsSettings.Instance.BannerPosition);
+        }
+
+        private void OnEnable()
+        {
+            if (AppAdsSettings.Instance.UseBanner)
+                IronSourceEvents.onBannerAdLoadedEvent += OnBannerAdLoadedEvent;
+        }
+
+        private void OnDisable()
+        {
+            if (AppAdsSettings.Instance.UseBanner)
+                IronSourceEvents.onBannerAdLoadedEvent -= OnBannerAdLoadedEvent;
+        }
+
+        private void OnBannerAdLoadedEvent()
+        {
+            Debug.Log("Banner Loaded");
+            IronSource.Agent.displayBanner();
+        }
     }
 }
---
+++ Assets/Runtime/Scripts/AppAdsSettings.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+namespace Ads
+{
+    [CreateAssetMenu(fileName = "App Ads Settings", menuName = "MrTinoTano/Ads/Ads Settings")]
+    public class AppAdsSettings : SingletonScriptableObject<AppAdsSettings>
+    {
+        public string AndroidAppID;
+        public string iOSAppID;
+
+        [Header("Banner Settings")]
+        public bool UseBanner;
+        [ConditionalHide("UseBanner")] public IronSourceBannerSize BannerSize;
+        [ConditionalHide("UseBanner")] public IronSourceBannerPosition BannerPosition;
+    }
+}
